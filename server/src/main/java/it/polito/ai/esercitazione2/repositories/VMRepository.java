2c7d3d08628363bcd4b75be07cc228972aeaf9a8
giuseppe.pastore <s257649@studenti.polito.it>
2020-07-16 16:11:28 +0200

    Updated getByTeam

--- server/src/main/java/it/polito/ai/esercitazione2/repositories/VMRepository.java
+++ server/src/main/java/it/polito/ai/esercitazione2/repositories/VMRepository.java
@@ -1,8 +1,14 @@
 package it.polito.ai.esercitazione2.repositories;
 
+import it.polito.ai.esercitazione2.entities.Course;
+import it.polito.ai.esercitazione2.entities.Team;
 import it.polito.ai.esercitazione2.entities.Token;
 import it.polito.ai.esercitazione2.entities.VM;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface VMRepository extends JpaRepository<VM,Long> {
+
+    List<VM> getByTeam(Team t);
 }
