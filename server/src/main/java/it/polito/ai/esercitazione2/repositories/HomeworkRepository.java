5557b4cd2a826156321c4c099f748d3680fd9921
Damiano Fisicaro <damiano.fisicaro@gmail.com>
2020-07-23 11:17:37 +0200

    Added Homework repo

---
+++ server/src/main/java/it/polito/ai/esercitazione2/repositories/HomeworkRepository.java
@@ -0,0 +1,17 @@
+package it.polito.ai.esercitazione2.repositories;
+
+import it.polito.ai.esercitazione2.entities.Assignment;
+import it.polito.ai.esercitazione2.entities.Homework;
+import it.polito.ai.esercitazione2.entities.HomeworkId;
+import it.polito.ai.esercitazione2.entities.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface HomeworkRepository extends JpaRepository<Homework, HomeworkId> {
+    @Query("SELECT h FROM Homework h WHERE h.assignment=:assignment")
+    List<Homework> getHomeworkByAssignment(Assignment assignment);
+    @Query("SELECT h FROM Homework h WHERE h.student=:student")
+    List<Homework> getHomeworkByStudent(Student student);
+}
