5a6f867b2d11ec3a5f65db444e785ea8096f2583
dpstart <danielepaliotta96@gmail.com>
2020-09-15 14:05:30 +0200

    Improve homepage

--- client/src/app/home.component.ts
+++ client/src/app/home.component.ts
@@ -1,17 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-home',
   template: `
-  <mat-card><h1>Select a course to continue</h1></mat-card>
+  <mat-card *ngIf="isLoggedIn()"><h1>Select a course to continue</h1></mat-card>
+  <mat-card *ngIf="!isLoggedIn()"><h1>Log in or Register to Continue</h1></mat-card>
   `,
   styles: ['mat-card{text-align:center}']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth: AuthService) { }
 
   ngOnInit(): void {
+  }
+
+  isLoggedIn() {
+    return this.auth.isLoggedIn()
   }
 
 }
