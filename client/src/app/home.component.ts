32dc78e0f5b82099f9d3fa3303a70537430ac254
dpstart <danielepaliotta96@gmail.com>
2020-09-20 14:44:08 +0200

    add auth buttons to homepage

--- client/src/app/home.component.ts
+++ client/src/app/home.component.ts
@@ -6,8 +6,21 @@ import { AuthService } from './services/auth.service';
   template: `
   <mat-card *ngIf="isLoggedIn()"><h1>Select a course to continue</h1></mat-card>
   <mat-card *ngIf="!isLoggedIn()"><h1>Log in or Register to Continue</h1></mat-card>
+
+  <div class="main-div">
+  <mat-card *ngIf="!isLoggedIn()" style="width:50%;">
+  <button style="margin-right:10px" mat-raised-button color="primary" mat-button [routerLink]="['.']" [queryParams]="{doLogin: true}">Login</button>
+  <button mat-raised-button color="primary" [routerLink]="['.']" [queryParams]="{doRegister: true}">Register</button>
+  </mat-card>
+  </div>
   `,
-  styles: ['mat-card{text-align:center; margin:20px}']
+  styles: [`
+  mat-card{text-align:center; margin:20px} 
+  .main-div{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }`]
 })
 export class HomeComponent implements OnInit {
 
